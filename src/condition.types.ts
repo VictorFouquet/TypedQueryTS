81696e38170a14d59919c1430183ecdbd156056e
victor <victor.fouquet@gmail.com>
2024-10-28 23:10:35 +0100

    Handle nested arrays and arrays in nested objects

--- src/condition.types.ts
+++ src/condition.types.ts
@@ -1,6 +1,37 @@
 import { NumericalOperation, LiteralOperation, BooleanOperation } from "./operation.types";
 import { ListOperator } from "./operator.types";
-import { AtLeastOne, EntityScalarKeys, EntityListKeys } from "./utils.types";
+import { AtLeastOne, EntityScalarKeys, EntityListKeys, ExactlyOne, Primitive } from "./utils.types";
+
+
+// Primitive query
+export type PrimitiveQuery<V extends Primitive> =
+    V extends number | Date ?
+        NumericalOperation | number | Date :
+        V extends string ?
+            LiteralOperation | string :
+            V extends boolean ?
+                BooleanOperation | boolean :
+                never;
+
+export type ValueQuery<V> =
+    V extends Primitive ?
+            PrimitiveQuery<V> :
+            V extends (infer U)[] ?
+                ListQuery<U> :
+                V extends object ?
+                    EntityQuery<V> :
+                    never;
+
+// Entity query
+export type EntityQuery<T> = AtLeastOne<{
+    [K in keyof T]: ValueQuery<T[K]>
+}>
+
+// List query
+export type ListQuery<T> = ExactlyOne<{
+    [K in ListOperator]: ValueQuery<T>
+}>
+
 
 // Field condition types based on field type
 export type FieldCondition<V> = V extends number | Date
