17ae9468602d968a1281b5de0fe27e1e7527784b
victor <victor.fouquet@gmail.com>
2024-10-29 17:32:29 +0100

    Update numerical operation tests

--- test/operation.test.ts
+++ test/operation.test.ts
@@ -13,7 +13,7 @@ import {
 //---------------------------------------------------------------------- SingleNumericalOperationType
 
 function testSingleNumericalOperationType<K extends NumericalOperator>(
-    op: SingleNumericalOperationType<K>
+    op: SingleNumericalOperationType<K, number>
 ): void {}
 
 test('SingleNumericalOperationType should allow operation with only one operator', () => {
@@ -63,27 +63,68 @@ testSingleNumericalOperationType<"gte">({ "lte": 0 });
 
 //---------------------------------------------------------------------- DoubleNumericalOperationType
 
-function testDoubleNumericalOperationType<K extends NumericalOperator, KP extends NumericalOperator>(
-    op: DoubleNumericalOperationType<K, KP>
+function testDoubleNumericalOperationType<K extends NumericalOperator, KP extends NumericalOperator, V extends number|Date>(
+    op: DoubleNumericalOperationType<K, KP, V>
 ): void {}
 
 test('DoubleNumericalOperationType should allow operation with exactly two operators', () => {
-    expect(_ => testDoubleNumericalOperationType<"eq", "lt">({ "eq": 0, "lt": 0 }));
-    expect(_ => testDoubleNumericalOperationType<"eq", "lte">({ "eq": 0, "lte": 0 }));
-    expect(_ => testDoubleNumericalOperationType<"eq", "gt">({ "eq": 0, "gt": 0 }));
-    expect(_ => testDoubleNumericalOperationType<"eq", "gte">({ "eq": 0, "gte": 0 }));
+    // Tests for number bounds
+    expect(_ => testDoubleNumericalOperationType<"eq", "lt", number>({ "eq": 0, "lt": 0 }));
+    expect(_ => testDoubleNumericalOperationType<"eq", "lte", number>({ "eq": 0, "lte": 0 }));
+    expect(_ => testDoubleNumericalOperationType<"eq", "gt", number>({ "eq": 0, "gt": 0 }));
+    expect(_ => testDoubleNumericalOperationType<"eq", "gte", number>({ "eq": 0, "gte": 0 }));
 
-    expect(_ => testDoubleNumericalOperationType<"lt", "lte">({ "lt": 0, "lte": 0 }));
-    expect(_ => testDoubleNumericalOperationType<"lt", "gt">({ "lt": 0, "gt": 0 }));
-    expect(_ => testDoubleNumericalOperationType<"lt", "gte">({ "lt": 0, "gte": 0 }));
+    expect(_ => testDoubleNumericalOperationType<"lt", "lte", number>({ "lt": 0, "lte": 0 }));
+    expect(_ => testDoubleNumericalOperationType<"lt", "gt", number>({ "lt": 0, "gt": 0 }));
+    expect(_ => testDoubleNumericalOperationType<"lt", "gte", number>({ "lt": 0, "gte": 0 }));
 
-    expect(_ => testDoubleNumericalOperationType<"gt", "lte">({ "gt": 0, "lte": 0 }));
-    expect(_ => testDoubleNumericalOperationType<"gt", "gte">({ "gt": 0, "gte": 0 }));
+    expect(_ => testDoubleNumericalOperationType<"gt", "lte", number>({ "gt": 0, "lte": 0 }));
+    expect(_ => testDoubleNumericalOperationType<"gt", "gte", number>({ "gt": 0, "gte": 0 }));
 
-    expect(_ => testDoubleNumericalOperationType<"gte", "lte">({ "gte": 0, "lte": 0 }));
+    expect(_ => testDoubleNumericalOperationType<"gte", "lte", number>({ "gte": 0, "lte": 0 }));
+
+    // Tests for Date bounds
+    expect(_ => testDoubleNumericalOperationType<"eq", "lt", Date>({ "eq": new Date(), "lt": new Date() }));
+    expect(_ => testDoubleNumericalOperationType<"eq", "lte", Date>({ "eq": new Date(), "lte": new Date() }));
+    expect(_ => testDoubleNumericalOperationType<"eq", "gt", Date>({ "eq": new Date(), "gt": new Date() }));
+    expect(_ => testDoubleNumericalOperationType<"eq", "gte", Date>({ "eq": new Date(), "gte": new Date() }));
+
+    expect(_ => testDoubleNumericalOperationType<"lt", "lte", Date>({ "lt": new Date(), "lte": new Date() }));
+    expect(_ => testDoubleNumericalOperationType<"lt", "gt", Date>({ "lt": new Date(), "gt": new Date() }));
+    expect(_ => testDoubleNumericalOperationType<"lt", "gte", Date>({ "lt": new Date(), "gte": new Date() }));
+
+    expect(_ => testDoubleNumericalOperationType<"gt", "lte", Date>({ "gt": new Date(), "lte": new Date() }));
+    expect(_ => testDoubleNumericalOperationType<"gt", "gte", Date>({ "gt": new Date(), "gte": new Date() }));
+
+    expect(_ => testDoubleNumericalOperationType<"gte", "lte", Date>({ "gte": new Date(), "lte": new Date() }));
 })
 
-// Should throw if same key is provided twice
+// Invalid for inconsistency in provided value types
+
+// @ts-expect-error
+testDoubleNumericalOperationType<"eq", "lt", number>({ "eq": 0, "lt": new Date() });
+// @ts-expect-error
+testDoubleNumericalOperationType<"eq", "lte", Date>({ "eq": 0, "lte": new Date() });
+// @ts-expect-error
+testDoubleNumericalOperationType<"eq", "gt", number>({ "eq": 0, "gt": new Date() });
+// @ts-expect-error
+testDoubleNumericalOperationType<"eq", "gte", Date>({ "eq": 0, "gte": new Date() });
+// @ts-expect-error
+testDoubleNumericalOperationType<"lt", "lte", number>({ "lt": 0, "lte": new Date() });
+// @ts-expect-error
+testDoubleNumericalOperationType<"lt", "gt", Date>({ "lt": 0, "gt": new Date() });
+// @ts-expect-error
+testDoubleNumericalOperationType<"lt", "gte", number>({ "lt": 0, "gte": new Date() });
+// @ts-expect-error
+testDoubleNumericalOperationType<"gt", "lte", Date>({ "gt": 0, "lte": new Date() });
+// @ts-expect-error
+testDoubleNumericalOperationType<"gt", "gte", number>({ "gt": 0, "gte": new Date() });
+// @ts-expect-error
+testDoubleNumericalOperationType<"gte", "lte", Date>({ "gte": 0, "lte": new Date() });
+
+
+// Invalid for providing same key twice
+
 // @ts-expect-error
 testDoubleNumericalOperationType<"eq", "eq">({ "eq": 0, "eq": 0 });
 // @ts-expect-error
@@ -112,71 +153,125 @@ testDoubleNumericalOperationType<"gte", "lte">({ "gt": 0, "lt": 0 });
 
 //---------------------------------------------------------------------- SingleNumericalOperation
 
-function testSingleNumericalOperation(op: SingleNumericalOperations): void {}
+function testSingleNumericalOperation<T extends number|Date>(op: SingleNumericalOperations<T>): void {}
 
-test('SingleNumericalOperation should allow operation with only one operator', () => {
-    expect(_ => testSingleNumericalOperation({ "eq": 0 }));
-    expect(_ => testSingleNumericalOperation({ "lt": 0 }));
-    expect(_ => testSingleNumericalOperation({ "gt": 0 }));
-    expect(_ => testSingleNumericalOperation({ "lte": 0 }));
-    expect(_ => testSingleNumericalOperation({ "gte": 0 }));
+test('SingleNumericalOperation should allow operation with only one operator associated to a number', () => {
+    expect(_ => testSingleNumericalOperation<number>({ "eq": 0 }));
+    expect(_ => testSingleNumericalOperation<number>({ "lt": 0 }));
+    expect(_ => testSingleNumericalOperation<number>({ "gt": 0 }));
+    expect(_ => testSingleNumericalOperation<number>({ "lte": 0 }));
+    expect(_ => testSingleNumericalOperation<number>({ "gte": 0 }));
 
-    expect(_ => testSingleNumericalOperation({ "eq": new Date() }));
-    expect(_ => testSingleNumericalOperation({ "lt": new Date() }));
-    expect(_ => testSingleNumericalOperation({ "gt": new Date() }));
-    expect(_ => testSingleNumericalOperation({ "lte": new Date() }));
-    expect(_ => testSingleNumericalOperation({ "gte": new Date() }));
+    expect(_ => testSingleNumericalOperation<Date>({ "eq": new Date() }));
+    expect(_ => testSingleNumericalOperation<Date>({ "lt": new Date() }));
+    expect(_ => testSingleNumericalOperation<Date>({ "gt": new Date() }));
+    expect(_ => testSingleNumericalOperation<Date>({ "lte": new Date() }));
+    expect(_ => testSingleNumericalOperation<Date>({ "gte": new Date() }));
 })
 
-// @ts-expect-error
-testSingleNumericalOperation({ "eq": true });
-// @ts-expect-error
-testSingleNumericalOperation({ "eq": "" });
-// @ts-expect-error
-testSingleNumericalOperation({ "eq": [] });
+// Invalid for associating a date to a set to be number field
 
 // @ts-expect-error
-testSingleNumericalOperation({ "eq": 0, "lt": 0 });
+testSingleNumericalOperation<number>({ "eq": new Date() });
 // @ts-expect-error
-testSingleNumericalOperation({ "eq": 0, "gt": 0 });
+testSingleNumericalOperation<number>({ "lt": new Date() });
 // @ts-expect-error
-testSingleNumericalOperation({ "eq": 0, "lte": 0 });
+testSingleNumericalOperation<number>({ "gt": new Date() });
 // @ts-expect-error
-testSingleNumericalOperation({ "eq": 0, "gte": 0 });
+testSingleNumericalOperation<number>({ "lte": new Date() });
+// @ts-expect-error
+testSingleNumericalOperation<number>({ "gte": new Date() });
+
+// Invalid for associating a number to a set to be Date field
 
 // @ts-expect-error
-testSingleNumericalOperation({ "lt": 0, "gt": 0 });
+testSingleNumericalOperation<Date>({ "eq": 0 });
 // @ts-expect-error
-testSingleNumericalOperation({ "lt": 0, "lte": 0 });
+testSingleNumericalOperation<Date>({ "lt": 0 });
 // @ts-expect-error
-testSingleNumericalOperation({ "lt": 0, "gte": 0 });
+testSingleNumericalOperation<Date>({ "gt": 0 });
+// @ts-expect-error
+testSingleNumericalOperation<Date>({ "lte": 0 });
+// @ts-expect-error
+testSingleNumericalOperation<Date>({ "gte": 0 });
+
+// Invalid for other wrong types
 
 // @ts-expect-error
-testSingleNumericalOperation({ "gt": 0, "lte": 0 });
+testSingleNumericalOperation<number>({ "eq": true });
 // @ts-expect-error
-testSingleNumericalOperation({ "gt": 0, "gte": 0 });
+testSingleNumericalOperation<number>({ "eq": "" });
+// @ts-expect-error
+testSingleNumericalOperation<number>({ "eq": [] });
+
+// Invalid combination with exclusive eq
 
 // @ts-expect-error
-testSingleNumericalOperation({ "gte": 0, "lte": 0 });
+testSingleNumericalOperation<number>({ "eq": 0, "lt": 0 });
+// @ts-expect-error
+testSingleNumericalOperation<number>({ "eq": 0, "gt": 0 });
+// @ts-expect-error
+testSingleNumericalOperation<number>({ "eq": 0, "lte": 0 });
+// @ts-expect-error
+testSingleNumericalOperation<number>({ "eq": 0, "gte": 0 });
+
+// Invalid combinations with incompatible bounds
+
+// @ts-expect-error
+testSingleNumericalOperation<number>({ "lt": 0, "gt": 0 });
+// @ts-expect-error
+testSingleNumericalOperation<number>({ "lt": 0, "lte": 0 });
+// @ts-expect-error
+testSingleNumericalOperation<number>({ "lt": 0, "gte": 0 });
+
+// @ts-expect-error
+testSingleNumericalOperation<number>({ "gt": 0, "lte": 0 });
+// @ts-expect-error
+testSingleNumericalOperation<number>({ "gt": 0, "gte": 0 });
+
+// @ts-expect-error
+testSingleNumericalOperation<number>({ "gte": 0, "lte": 0 });
 
 
 //---------------------------------------------------------------------- DoubleNumericalOperation
 
-function testDoubleNumericalOperation(op: DoubleNumericalOperations): void {}
+function testDoubleNumericalOperation<T extends number | Date>(op: DoubleNumericalOperations<T>): void {}
 
 test('DoubleNumericalOperations should allow any legal combination of two operators', () => {
-    expect(_ => testDoubleNumericalOperation({ "lt": 0, "gt": 0 }));
-    expect(_ => testDoubleNumericalOperation({ "lt": 0, "gte": 0 }));
+    expect(_ => testDoubleNumericalOperation<number>({ "lt": 0, "gt": 0 }));
+    expect(_ => testDoubleNumericalOperation<number>({ "lt": 0, "gte": 0 }));
 
-    expect(_ => testDoubleNumericalOperation({ "lte": 0, "gt": 0 }));
-    expect(_ => testDoubleNumericalOperation({ "lte": 0, "gte": 0 }));
+    expect(_ => testDoubleNumericalOperation<number>({ "lte": 0, "gt": 0 }));
+    expect(_ => testDoubleNumericalOperation<number>({ "lte": 0, "gte": 0 }));
 
-    expect(_ => testDoubleNumericalOperation({ "lt": new Date(), "gt": new Date() }));
-    expect(_ => testDoubleNumericalOperation({ "lt": new Date(), "gte": new Date() }));
+    expect(_ => testDoubleNumericalOperation<Date>({ "lt": new Date(), "gt": new Date() }));
+    expect(_ => testDoubleNumericalOperation<Date>({ "lt": new Date(), "gte": new Date() }));
 
-    expect(_ => testDoubleNumericalOperation({ "lte": new Date(), "gt": new Date() }));
-    expect(_ => testDoubleNumericalOperation({ "lte": new Date(), "gte": new Date() }));
+    expect(_ => testDoubleNumericalOperation<Date>({ "lte": new Date(), "gt": new Date() }));
+    expect(_ => testDoubleNumericalOperation<Date>({ "lte": new Date(), "gte": new Date() }));
 })
+
+// Invalid numerical Date-number associations with valid bounds
+
+// @ts-expect-error
+testDoubleNumericalOperation<Date>({ "lt": 0, "gt": 0 });
+// @ts-expect-error
+testDoubleNumericalOperation<Date>({ "lt": 0, "gte": 0 });
+// @ts-expect-error
+testDoubleNumericalOperation<Date>({ "lte": 0, "gt": 0 });
+// @ts-expect-error
+testDoubleNumericalOperation<Date>({ "lte": 0, "gte": 0 });
+
+// @ts-expect-error
+testDoubleNumericalOperation<number>({ "lt": new Date(), "gt": new Date() });
+// @ts-expect-error
+testDoubleNumericalOperation<number>({ "lt": new Date(), "gte": new Date() });
+// @ts-expect-error
+testDoubleNumericalOperation<number>({ "lte": new Date(), "gt": new Date() });
+// @ts-expect-error
+testDoubleNumericalOperation<number>({ "lte": new Date(), "gte": new Date() });
+
+// Invalid combinations for using eq
 
 // @ts-expect-error
 testDoubleNumericalOperation({ "eq": 0, "lt": 0 });
@@ -187,10 +282,14 @@ testDoubleNumericalOperation({ "eq": 0, "lte": 0 });
 // @ts-expect-error
 testDoubleNumericalOperation({ "eq": 0, "gte": 0 });
 
+// Invalid bound combinations
+
 // @ts-expect-error
 testDoubleNumericalOperation({ "lt": 0, "lte": 0 });
 // @ts-expect-error
 testDoubleNumericalOperation({ "gt": 0, "gte": 0 });
+
+// Invalid type associations
 
 // @ts-expect-error
 testDoubleNumericalOperation({ "lt": true });
@@ -203,33 +302,35 @@ testDoubleNumericalOperation({ "lt": 0, "gt": "" });
 
 //---------------------------------------------------------------------- NumericalOperation
 
-function testNumericalOperation(op: NumericalOperation): void {}
+function testNumericalOperation<V extends number|Date>(op: NumericalOperation<V>): void {}
 
 test('NumericalOperation should allow only valid numerical operator combination or single operator', () => {
-    expect(_ => testNumericalOperation({ "eq": 0 }));
-    expect(_ => testNumericalOperation({ "lt": 0 }));
-    expect(_ => testNumericalOperation({ "gt": 0 }));
-    expect(_ => testNumericalOperation({ "lte": 0 }));
-    expect(_ => testNumericalOperation({ "gte": 0 }));
+    expect(_ => testNumericalOperation<number>({ "eq": 0 }));
+    expect(_ => testNumericalOperation<number>({ "lt": 0 }));
+    expect(_ => testNumericalOperation<number>({ "gt": 0 }));
+    expect(_ => testNumericalOperation<number>({ "lte": 0 }));
+    expect(_ => testNumericalOperation<number>({ "gte": 0 }));
 
-    expect(_ => testNumericalOperation({ "lt": 0, "gt": 0 }));
-    expect(_ => testNumericalOperation({ "lt": 0, "gte": 0 }));
+    expect(_ => testNumericalOperation<number>({ "lt": 0, "gt": 0 }));
+    expect(_ => testNumericalOperation<number>({ "lt": 0, "gte": 0 }));
 
-    expect(_ => testNumericalOperation({ "lte": 0, "gt": 0 }));
-    expect(_ => testNumericalOperation({ "lte": 0, "gte": 0 }));
+    expect(_ => testNumericalOperation<number>({ "lte": 0, "gt": 0 }));
+    expect(_ => testNumericalOperation<number>({ "lte": 0, "gte": 0 }));
 
-    expect(_ => testNumericalOperation({ "eq": new Date() }));
-    expect(_ => testNumericalOperation({ "lt": new Date() }));
-    expect(_ => testNumericalOperation({ "gt": new Date() }));
-    expect(_ => testNumericalOperation({ "lte": new Date() }));
-    expect(_ => testNumericalOperation({ "gte": new Date() }));
+    expect(_ => testNumericalOperation<Date>({ "eq": new Date() }));
+    expect(_ => testNumericalOperation<Date>({ "lt": new Date() }));
+    expect(_ => testNumericalOperation<Date>({ "gt": new Date() }));
+    expect(_ => testNumericalOperation<Date>({ "lte": new Date() }));
+    expect(_ => testNumericalOperation<Date>({ "gte": new Date() }));
 
-    expect(_ => testNumericalOperation({ "lt": new Date(), "gt": new Date() }));
-    expect(_ => testNumericalOperation({ "lt": new Date(), "gte": new Date() }));
+    expect(_ => testNumericalOperation<Date>({ "lt": new Date(), "gt": new Date() }));
+    expect(_ => testNumericalOperation<Date>({ "lt": new Date(), "gte": new Date() }));
 
-    expect(_ => testNumericalOperation({ "lte": new Date(), "gt": new Date() }));
-    expect(_ => testNumericalOperation({ "lte": new Date(), "gte": new Date() }));
+    expect(_ => testNumericalOperation<Date>({ "lte": new Date(), "gt": new Date() }));
+    expect(_ => testNumericalOperation<Date>({ "lte": new Date(), "gte": new Date() }));
 })
+
+// Invalid for combining with exclusive "eq"
 
 // @ts-expect-error
 testNumericalOperation({ "eq": 0, "lt": 0 });
@@ -240,10 +341,14 @@ testNumericalOperation({ "eq": 0, "lte": 0 });
 // @ts-expect-error
 testNumericalOperation({ "eq": 0, "gte": 0 });
 
+// Invalid bounds combinations
+
 // @ts-expect-error
 testNumericalOperation({ "lt": 0, "lte": 0 });
 // @ts-expect-error
 testNumericalOperation({ "gt": 0, "gte": 0 });
+
+// Invalid provided types
 
 // @ts-expect-error
 testNumericalOperation({ "lt": true });
