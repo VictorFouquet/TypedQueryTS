e5e3856dfbc87111eb6a82d3e3ed1d46b45ded93
victor <victor.fouquet@gmail.com>
2024-11-01 19:35:39 +0100

    Add main query wrapper type

---
+++ src/query.types.ts
@@ -0,0 +1,5 @@
+import { WhereClause } from "./condition.types";
+import { OrderByClause } from "./ordering.types";
+import { SelectClause } from "./selection.types";
+
+export type Query<T> = SelectClause<T> | WhereClause<T> | OrderByClause<T>;
---
+++ test/query.test.ts
@@ -0,0 +1,63 @@
+import { expectTypeOf, test } from 'vitest';
+
+import { Query } from "../src/query.types";
+
+
+interface Entity {
+    name: string,
+    nested: {
+        id: number
+    }
+}
+
+test('Query should handle select clause', () => {
+    expectTypeOf<{
+        select: {
+            name: true,
+            nested: {
+                id: true
+            }
+        }
+    }>().toMatchTypeOf<Query<Entity>>()
+});
+
+test('Query should handle where clause', () => {
+    expectTypeOf<{
+        where: {
+            name: "John",
+            nested: {
+                id: { gt: 0, lt: 10}
+            }
+        }
+    }>().toMatchTypeOf<Query<Entity>>()
+});
+
+test('Query should handle orderBy clause', () => {
+    expectTypeOf<{
+        orderBy: [
+            { name: "asc" },
+            { nested: { id: "desc" } }
+        ]
+    }>().toMatchTypeOf<Query<Entity>>()
+});
+
+test('Query should handle combination of different clauses', () => {
+    expectTypeOf<{
+        select: {
+            name: true,
+            nested: {
+                id: true
+            }
+        },
+        where: {
+            name: "John",
+            nested: {
+                id: { gt: 0, lt: 10}
+            }
+        },
+        orderBy: [
+            { name: "asc" },
+            { nested: { id: "desc" } }
+        ]
+    }>().toMatchTypeOf<Query<Entity>>()
+});
